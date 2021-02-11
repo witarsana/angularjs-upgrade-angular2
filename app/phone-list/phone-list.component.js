cfc38d1e021c4a62d62f5c25c6b308007e23ccef
Witarsana <made.witarsana@gmail.com>
2021-02-11 15:05:08 +0800

    modify phone-list component into ts and change controller into class style

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,14 +1,13 @@
-'use strict';
-
-// Register `phoneList` component, along with its associated controller and template
-angular.
-  module('phoneList').
-  component('phoneList', {
-    templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['Phone',
-      function PhoneListController(Phone) {
+class PhoneListController {
+    constructor(Phone) {
         this.phones = Phone.query();
-        this.orderProp = 'age';
-      }
-    ]
-  });
+        this.orderProp = "age";
+    }
+}
+PhoneListController.$inject = ["Phone"];
+// Register `phoneList` component, along with its associated controller and template
+angular.module("phoneList").component("phoneList", {
+    templateUrl: "phone-list/phone-list.template.html",
+    controller: PhoneListController,
+});
+//# sourceMappingURL=phone-list.component.js.map
