b5cf7e63b1e1357d34eff3557f29c66bbaecb812
Witarsana <made.witarsana@gmail.com>
2021-02-11 15:37:09 +0800

    modify the bootstrap way of angular

--- app/app.module.js
+++ app/app.module.js
@@ -5,15 +5,26 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
 const platform_browser_1 = require("@angular/platform-browser");
+const static_1 = require("@angular/upgrade/static");
 let AppModule = class AppModule {
+    constructor(upgrade) {
+        this.upgrade = upgrade;
+    }
+    ngDoBootstrap() {
+        this.upgrade.bootstrap(document.documentElement, ["phonecatApp"]);
+    }
 };
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule],
-    })
+        imports: [platform_browser_1.BrowserModule, static_1.UpgradeModule],
+    }),
+    __metadata("design:paramtypes", [static_1.UpgradeModule])
 ], AppModule);
 exports.AppModule = AppModule;
 //# sourceMappingURL=app.module.js.map
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { UpgradeModule } from "@angular/upgrade/static";
 
 @NgModule({
-  imports: [BrowserModule],
+  imports: [BrowserModule, UpgradeModule],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private upgrade: UpgradeModule) {}
+  ngDoBootstrap() {
+    this.upgrade.bootstrap(document.documentElement, ["phonecatApp"]);
+  }
+}
